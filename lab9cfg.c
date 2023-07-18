7aa8573b6ede72c552ca09187a39056e857b44ad
Shikari0407 <biraj2019pudasaini@gmail.com>
2023-07-18 17:43:01 +0545

    all Toc lab added

---
+++ lab9cfg.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the value of n (n >= 1): ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Invalid input! n must be greater than or equal to 1.\n");
+        return 0;
+    }
+
+    printf("Generated string for the imput %d is: ",n);
+
+    // Generate the string "a^n b^n c^n"
+    for (int i = 0; i < n; i++) {
+        printf("a");
+    }
+    for (int i = 0; i < n; i++) {
+        printf("b");
+    }
+    for (int i = 0; i < n; i++) {
+        printf("c");
+    }
+
+    printf("\n");
+
+    return 0;
+}
